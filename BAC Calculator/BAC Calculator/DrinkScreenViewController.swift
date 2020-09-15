eb6836efbd68c12201eb51431dc9f4ac2842fe0e
tyandow <46328641+tyandow@users.noreply.github.com>
2020-09-15 16:17:59 -0400

    Added counting drink functions

--- BAC Calculator/BAC Calculator/DrinkScreenViewController.swift
+++ BAC Calculator/BAC Calculator/DrinkScreenViewController.swift
@@ -9,9 +9,49 @@
 import UIKit
 
 class DrinkScreenViewController: UIViewController {
-
+    
+    @IBOutlet weak var bacLevel: UILabel!
+    var ounces : Double = 0
+    var UserInforVC = UserInfoViewCotroller()
+    var genderCof : Double = 0.0
+    var userWeight : Double = 0
+    var bac : Double = 0.0
+    var count : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bacLevel.text = "0"
+        ounces = 0
+    }
+    
+    func calculateBAC(){
+        genderCof = UserInforVC.genderNumValue
+        userWeight = Double(UserInforVC.weight)!
+        bac = ((ounces)*(genderCof))/(userWeight)
+        bacLevel.text = String(bac)
+    }
+    
+    @IBAction func onTappedBeer(_ sender: Any) {
+        //print("beer")
+        //ounces = ounces + 12
+        //calculateBAC()
+        count = count + 1
+        bacLevel.text = String(count)
+    }
+    
+    @IBAction func onTappedWine(_ sender: Any) {
+        //print("wine")
+        //ounces = ounces + 5
+        //calculateBAC()
+        count = count + 1
+        bacLevel.text = String(count)
+    }
+    
+    @IBAction func onTappedShot(_ sender: Any) {
+        //print("shot")
+        //ounces = ounces + 1
+        //calculateBAC()
+        count = count + 1
+        bacLevel.text = String(count)
     }
 }
--- BAC Calculator/BAC Calculator/UserInfoViewController.swift
+++ BAC Calculator/BAC Calculator/UserInfoViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class UserInfoViewCotroller: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var birthdayField: UITextField!
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var genderOption: UISegmentedControl!
+    var name : String = ""
+    var email : String = ""
+    var username : String = ""
+    var birthday : String = ""
+    var height : String = ""
+    var weight : String = ""
+    var gender : Int = 3
+    var genderNumValue : Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
     @IBAction func onTappedCheers(_ sender: Any) {
         //self.dismiss(animated: true, completion:nil)
         performSegue(withIdentifier: "toDrinks", sender: self)
+        name = nameField.text!
+        email = emailField.text!
+        username = usernameField.text!
+        birthday = birthdayField.text!
+        height = heightField.text!
+        weight = weightField.text!
+        gender = genderOption.selectedSegmentIndex
+        if(gender == 0){
+            genderNumValue = 3.75
+        }else{
+            genderNumValue = 4.7
+        }
+     
     }
 }
