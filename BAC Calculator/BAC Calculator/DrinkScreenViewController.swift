84296323c8da32139864d303afe709881e6b200a
tyandow <46328641+tyandow@users.noreply.github.com>
2020-10-28 19:41:42 -0400

    UI Updates

--- BAC Calculator/BAC Calculator/DrinkScreenViewController.swift
+++ BAC Calculator/BAC Calculator/DrinkScreenViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
 
 class DrinkScreenViewController: UIViewController {
+    
+    private let dataSource = ["Beer 1", "Beer 2", "Beer 3", "Beer 4", "Beer 5" ]
     
     @IBOutlet weak var bacLevel: UILabel!
     var ounces : Double = 0
@@ -21,7 +23,8 @@ class DrinkScreenViewController: UIViewController {
     @IBOutlet weak var doNotDrive: UILabel!
     @IBOutlet weak var skull: UIImageView!
     @IBOutlet weak var beerScroll: UIPickerView!
-    //var ref: DatabaseReference!
+    //var ref:FIRDatabaseReference = Database.database().reference()
+    //var ref: DatabaseReference?
     //ref = Database.database().reference()
     
     override func viewDidLoad() {
@@ -31,6 +34,8 @@ class DrinkScreenViewController: UIViewController {
         skull.isHidden = true
         beerScroll.isHidden = true
         doNotDrive.isHidden = true
+        beerScroll.dataSource = self
+        beerScroll.delegate = self
     }
     
     func calculateBAC(){
@@ -52,7 +57,7 @@ class DrinkScreenViewController: UIViewController {
         //print("beer")
         ounces = ounces + ((12)*(0.05))
         calculateBAC()
-        //beerScroll.isHidden = false
+        beerScroll.isHidden = false
         //count = count + 1
         //bacLevel.text = String(count)
     }
@@ -61,6 +66,7 @@ class DrinkScreenViewController: UIViewController {
         //print("wine")
         ounces = ounces + ((5)*(0.12))
         calculateBAC()
+        beerScroll.isHidden = true
         //count = count + 1
         //bacLevel.text = String(count)
     }
@@ -69,6 +75,7 @@ class DrinkScreenViewController: UIViewController {
         //print("shot")
         ounces = ounces + ((1)*(0.40))
         calculateBAC()
+        beerScroll.isHidden = true
         //count = count + 1
         //bacLevel.text = String(count)
     }
@@ -77,4 +84,23 @@ class DrinkScreenViewController: UIViewController {
         performSegue(withIdentifier: "showFriends", sender: self)
     }
     
+}
+
+extension DrinkScreenViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+   }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    //func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    //    detailLabel.text = dataSource[row ]
+    //}
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
 }
